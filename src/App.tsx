782801fe5dbd96e6128ea9dd0a7e92e35bb4209d
Orinea Tovhakale <tovhakaleo@gmail.com>
2025-10-29 15:51:58 +0200

    Initial commit of the the booking page on the dev_orinea branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
+
 import { LandingPage } from './Pages/Landing_Page/LandingPage';
 import { AdminDashboard } from './Pages/admin_dashboard/AdminDashboard';
 import { CustomerDashboard } from './Pages/Customer_Dashboard/CustomerDashboard';
@@ -11,44 +12,63 @@ import { Navbar } from './Components/Navbar/Navbar';
 import RoomDetails from "./Pages/Room_Details/RoomDetails";
 import HotelDetails from "./Pages/HotelDetails/HotelDetails";
 
+// Import BookingPage from the correct folder
+import BookingPage from './Pages/Booking_Page/BookingPage';
+
 function AppContent() {
   const location = useLocation();
+  // Only show the public Navbar on the landing page
   const showNavbar = location.pathname === '/';
 
   return (
     <>
       {showNavbar && <Navbar />}
+
       <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/hotel-details/:id" element={<HotelDetails />} />
-          <Route path="/room-details/:id" element={<RoomDetails />} />
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-                <CustomerDashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="/rooms" element={<CustomerDashboard />} />
-          <Route 
-            path="/admin/*" 
-            element={
-              <ProtectedRoute requireAdmin={true}>
-                <AdminDashboard />
-              </ProtectedRoute>
-            } 
-          />
-        </Routes>
-      </>
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/hotel-details/:id" element={<HotelDetails />} />
+        <Route path="/room-details/:id" element={<RoomDetails />} />
+
+        {/* Protected Booking Page */}
+        <Route
+          path="/booking"
+          element={
+            <ProtectedRoute>
+              <BookingPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Customer Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <CustomerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/rooms" element={<CustomerDashboard />} />
+
+        {/* Admin Dashboard */}
+        <Route
+          path="/admin/*"
+          element={
+            <ProtectedRoute requireAdmin={true}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
 function App() {
-  const basename = import.meta.env.PROD ? '/Mlab_Hotel_Team_Delta' : ''
-
+  const basename = import.meta.env.PROD ? '/Mlab_Hotel_Team_Delta' : '';
   return (
     <AuthProvider>
       <Router basename={basename}>
